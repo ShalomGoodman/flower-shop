de09338f22fba4da6539a406f20032a564afc2cf
username <youremail@domain.com>
2023-06-08 19:45:29 -0400

    HOT-FIX: font and color chamges

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,15 +7,15 @@ function Footer() {
             <img className="social-media-icon" src="https://i.imgur.com/KxfWH9J.png" alt="logo" />
             <h3>FLOWERBOX / EST.2019</h3>
         </div>
-        <div className="customer-service">
-            <h3>Customer Service</h3>
+        <div className="customer-service text1">
+            <h3>customer service</h3>
             <p>SATASFACTION COMMITMENT</p>
             <p>DELIVERY POLICIES</p>
             <p>CANCELLATIONS</p>
             <p>RETURN POLICY</p>
         </div>
-        <div className="flowerbox">
-            <h3>Flowerbox</h3>
+        <div className="flowerbox text1">
+            <h3>flowerbox</h3>
             <p>TERMS OF SERVICE</p>
             <p>PRIVACY POLICY</p>
             <p>CAREER OPERTUNITIES</p>
